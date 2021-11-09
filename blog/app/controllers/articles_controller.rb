b24b259767ccacfb99bcff10dc22fd04765da1fd
romerodevelopments <gandresrp@gmail.com>
2021-11-09 09:18:51 -0300

    primeros pasos - editar articulos

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -5,13 +5,26 @@ class ArticlesController < ApplicationController
     end
 
     def new 
-        @article = Article.new
-        @article.title = 'Demo'
+        @article = Article.new #envia un articulo nuevo al formlario de la vista
     end
 
     def create
         @article = Article.create(title: params[:article][:title], content: params[:article][:content])
         render json: @article
     end
+
+    def edit
+        @article = Article.find(params[:id])
+        puts "\n\n\n  #{@article.persisted?} \n\n\n"
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        
+        redirect_to @article
+    end
+
+    
     
 end
