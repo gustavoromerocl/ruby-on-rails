e17adb787f1d36fbefa3f4945832d53159aecd37
romerodevelopments <gandresrp@gmail.com>
2021-11-09 09:30:58 -0300

    primeros pasos - ruta base

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/bienvenida", to: "home#index"
 
+  root to: "home#index"
+
   get "articles/new", to: "articles#new" #controlador/accion
   get "articles/:id", to: "articles#show" 
   get "articles/:id/edit", to: "articles#edit"
