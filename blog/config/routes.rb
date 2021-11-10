75d31b08203901849c7a610ac5d70148168e318b
romerodevelopments <gandresrp@gmail.com>
2021-11-09 23:34:11 -0300

    autenticacion de usuarios - instalar devise

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/bienvenida", to: "home#index"
 
-  root to: "home#index"
+  root to: "articles#index"
 
   get "articles", to: "articles#index"
   get "articles/new", to: "articles#new", as: :new_articles #controlador/accion
