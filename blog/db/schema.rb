692d03de54cb74902d9f908edb553e1ec53db70b
romerodevelopments <gandresrp@gmail.com>
2021-11-18 10:31:31 -0300

    feature complete

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_174034) do
+ActiveRecord::Schema.define(version: 2021_11_18_105727) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_174034) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
